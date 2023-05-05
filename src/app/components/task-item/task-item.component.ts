400b5187384bd0c09b903df7e8c0b583257cac5c
Bart <44099512+bartdob@users.noreply.github.com>
2023-05-05 13:00:53 +0200

    add delete button

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import {Task} from '../../Task';
 import { faTimes, faTrash } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,7 +9,13 @@ import { faTimes, faTrash } from '@fortawesome/free-solid-svg-icons';
 })
 export class TaskItemComponent {
   @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new 
+  EventEmitter;
   faTimes = faTimes;
   faTrash = faTrash;
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
 
 }
